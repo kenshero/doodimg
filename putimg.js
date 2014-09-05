27828ef84c220afac73c80700a684fe9807844d7
kenshero <kenshero.k@gmail.com>
2014-09-05 17:14:43 +0700

    fix image

--- putimg.js
+++ putimg.js
@@ -1,28 +1,45 @@
 var https = require('https');
 var fs = require('fs');
 
-https.get("https://graph.facebook.com/v1.0/204098339714284/photos?type=uploaded&limit=200", function(response) {
-  var result = '';
-  response.on('data', function (chunk) {
-    result += chunk;
-  });
-  response.on('end', function (chunk) {
-    var photos = JSON.parse(result);
-    var count=0;
-    for(var i=0;i<photos.data.length;i++) {
-      var photo = photos.data[i];
-      console.log(photo.id);
-      ++count;
-      downloadImage(photo.id, photo.source);
+var url ="https://graph.facebook.com/SleepingForLessAndroidProgramming/photos?type=uploaded";
+
+getfb(url);
+
+  function getfb(url){
+
+    https.get(url, function(response) {
+      var result = '';
+      response.on('data', function (chunk) {
+        result += chunk;
+      });
+
+        response.on('end', function (chunk) {
+        var photos = JSON.parse(result);
+
+        for(var i=0;i<photos.data.length;i++) {
+          var photo = photos.data[i];
+          console.log(photo.id);
+          downloadImage(photo.id, photo.source);
+        }
+          console.log(photos.data.length);
+          
+          if(photos.paging.next !== undefined)
+           {
+            console.log(photos.paging.next);
+            getfb(photos.paging.next);
+           }
+           else{
+            console.log("gg");
+           }
+
+      });
+
+    });
+ }
+
+    function downloadImage(id, url) {
+      var file = fs.createWriteStream('./images/'+id + '.jpg');
+      var request = https.get(url, function(response) {
+        response.pipe(file);
+      });
     }
-    console.log(count);
-  });
-
-});
-
-function downloadImage(id, url) {
-  var file = fs.createWriteStream(id + '.jpg');
-  var request = https.get(url, function(response) {
-    response.pipe(file);
-  });
-}
